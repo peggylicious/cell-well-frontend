44fc1e7d7b1ca4d6553c0c843f186ceebad659e8
margaret ogbor <p.asq@live.com>
2024-01-01 09:29:36 +0100

    Find loggedin user

--- src/app/core/services/core.service.ts
+++ src/app/core/services/core.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable, NgZone } from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
 
@@ -5,14 +6,26 @@ import {MatSnackBar} from '@angular/material/snack-bar';
   providedIn: 'root'
 })
 export class CoreService {
+  baseUrl: string = 'http://localhost:5000/api/users'
 
-  constructor(private _snackBar: MatSnackBar, private zone: NgZone) { }
+  constructor(private _snackBar: MatSnackBar, private zone: NgZone, private http: HttpClient) { }
 
   openDialog(message:string, action:string){
     this.zone.run(() => {
-    this._snackBar.open(message, action, {verticalPosition: 'top', panelClass: ['background-red']
+      this._snackBar.open(message, action, {verticalPosition: 'top', panelClass: ['background-red']
 
-    });
-  })
+      });
+    })
+  }
+
+  findUser(){
+    return this.http.get(`${this.baseUrl}/getAllUsers`).subscribe({next: (res)=> {
+      console.log('User there', res)
+    },
+    error(err) {
+      console.log('User there', err)
+    },
+
+    })
   }
 }
