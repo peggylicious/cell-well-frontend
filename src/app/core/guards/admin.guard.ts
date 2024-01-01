fd18f55da3daa38e183c8993c4d8410248d0693f
margaret ogbor <p.asq@live.com>
2024-01-01 09:23:43 +0100

    Add admin guard

--- src/app/core/guards/admin.guard.ts
+++ src/app/core/guards/admin.guard.ts
@@ -1,8 +1,14 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  const loggedInUser = JSON.parse(localStorage.getItem('cwNewUser')!);
-  if(loggedInUser.role === 'Admin'){
+  // const loggedInUser = JSON.parse(localStorage.getItem('cwNewUser')!);
+  // const loggedInUser = inject(AuthenticationService)
+  const loggedInUser = JSON.parse(localStorage.getItem('cwNewUser') as string);
+
+  // if(loggedInUser.role === 'Admin'){
+  if(loggedInUser?.role === 'Basic'){
     return true
   }else{
     return false
